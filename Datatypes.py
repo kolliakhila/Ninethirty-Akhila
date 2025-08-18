008e5e0c5ef6e8debbf858b216534aa00157cd7d
kolliakhila <kolliakhila7@gmail.com>
2025-08-18 11:28:52 +0530

    Add Datatypes.py and hello.py

---
+++ Datatypes.py
@@ -0,0 +1,13 @@
+print("Different data types")
+a=2
+b=5.4
+c=True
+d="Akhila"
+print("Value of a is", a)
+print(type(a))
+print("Value of b is", b)
+print(type(b))
+print("Value of c is", c)
+print(type(c))
+print("Value of d is", d)
+print(type(d))
