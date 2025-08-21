8fca614ad663e15c498b79e6e46a01005ce95bc3
kolliakhila <kolliakhila7@gmail.com>
2025-08-21 11:08:09 +0530

    This is the file of Aritmetic operators

---
+++ Arithmeticoperator.py
@@ -0,0 +1,20 @@
+print("Arithmetic Operators")
+a=10
+b=2
+result=a + b
+print(result)
+
+result2=a - b
+print(result)
+
+
+result=a * b
+print(result)
+
+
+result=a / b
+print(result)
+
+
+result=a % b
+print(result)
